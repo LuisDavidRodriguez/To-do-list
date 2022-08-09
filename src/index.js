4f9ed3f0f711635bc6bfa5f68f5dd140087e514f
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-08-09 18:27:41 -0500

    Solve linters errors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const itemContainer = document.querySelector('#itemsContainer');
 const activitiesManager = new ActivitiesManager(itemContainer);
 
 activitiesManager.addTask('walk the doog', false, 0);
-activitiesManager.addTask('Cooking dinner', true, 1);
-activitiesManager.addTask('Study', false, 2);
+activitiesManager.addTask('Cooking dinner', false, 1);
+activitiesManager.addTask('Study', true, 2);
 activitiesManager.addTask('Go to the gym', true, 3);
 activitiesManager.displayTasks();
