e0ec4116e3f51ffd04fb6ca2e75074a47afc4874
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-08-09 11:18:41 -0500

    set up

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+/* eslint-disable prefer-destructuring */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'head',
+      scriptLoading: 'defer',
+    }),
+  ],
+
+  devServer: {
+    port: 9000,
+    static: './dist',
+    compress: true,
+    historyApiFallback: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
